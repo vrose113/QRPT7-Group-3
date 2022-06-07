8e1dab389fc348453caa252f547f6cff09f16d9c
Victoria <vrfriedm@gmail.com>
2022-06-06 19:08:29 -0700

    add homePage

---
+++ homePage.ts
@@ -0,0 +1,18 @@
+import {By} from 'selenium-webdriver'
+import {BasePage} from './basePage'
+
+export class Google extends BasePage {
+    //add selectors below this line and above the constructor line!!//
+
+
+
+
+
+
+
+
+    
+    constructor() {
+        super({url: "https://www.pizzahut.com/"})
+    }
+}
